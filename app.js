921c7fcf3e4f05da839d9dc47406534a598e3c6a
Josh Glazebrook <Josh@d-205-175-113-230.dhcp4.washington.edu>
2014-05-06 17:03:10 -0700

    tesT

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ server.route([
 function sendMessage(req, res) {
 // curl -X POST -d "to=+14155550000&message=This is a test from Blower.io" -H "Accept: application/json" https://0e4fbd3a-37d3-4639-86d6-17920cf903f5:Tt8pSc7CbgHul3aPgCLevg@api.blower.io/messages
     console.log(process.env);
+    res(process.env);
 }
 
 function doTest(req, res) {
