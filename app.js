59ddeea230cd4b7c2e47011351c6670bcc5b8189
Josh Glazebrook <Josh@d-205-175-113-230.dhcp4.washington.edu>
2014-05-06 17:02:35 -0700

    tesT

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 var hapi = require('hapi');
 
-var server = new hapi.Server(process.env.PORT);
+var server = new hapi.Server(process.env.PORT || 3000);
+var request = require('request');
 
 
 server.route([
     { method: 'GET', path: '/test', config: { handler: doTest }},
-    { method: 'GET', path: '/names', config: { handler: listofcrap }}
+    { method: 'GET', path: '/names', config: { handler: listofcrap }},
+    { method: 'GET', path: '/send', config: { handler: sendMessage }}
 ]);
+
+
+function sendMessage(req, res) {
+// curl -X POST -d "to=+14155550000&message=This is a test from Blower.io" -H "Accept: application/json" https://0e4fbd3a-37d3-4639-86d6-17920cf903f5:Tt8pSc7CbgHul3aPgCLevg@api.blower.io/messages
+    console.log(process.env);
+}
 
 function doTest(req, res) {
     res("Something");
