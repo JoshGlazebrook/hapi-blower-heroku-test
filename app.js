31d5edba892c87a797f74f1f0128d8b4333080fa
Josh Glazebrook <Josh@d-205-175-113-230.dhcp4.washington.edu>
2014-05-06 17:36:27 -0700

    test

--- app.js
+++ app.js
@@ -5,46 +5,45 @@ var request = require('request');
 
 
 server.route([
-    { method: 'GET', path: '/test', config: { handler: doTest }},
-    { method: 'GET', path: '/names', config: { handler: listofcrap }},
-    { method: 'GET', path: '/send', config: { handler: sendMessage }}
+    { method: 'GET', path: '/{path*}', config: { handler: { directory: { path: './public', listing: false, index: true }}}},
+    { method: 'POST', path: '/send', config: { handler: sendMessage }}
 ]);
 
 
 function sendMessage(req, res) {
 
 
-    var message = "finish the fucking project";
+    /*var message = "finish the fucking project";
 
     var numbers = [
         '4257738068',
         '3605562243',
         '2066975995',
         '2064035209'
-    ];
+    ];*/
 
-    numbers.forEach(function(item) {
+    /*numbers.forEach(function(item) {
         request.post(process.env.BLOWERIO_URL + '/messages', { form: { to: item, message: message }, headers: { 'Accept': 'application/json' } }, function (err, res, body) {
             console.log(body);
         });
 
         res("messages sent");
+    });*/
+
+    var data = {
+        to: req.payload.number,
+        message: req.payload.message
+    };
+
+    request.post(process.env.BLOWERIO_URL + '/messages', { form: data, headers: { 'Accept': 'application/json' }}, function(err, response, body) {
+        body = JSON.parse(body);
+
+        if (body.message === "ok")
+            res("Message sent!");
+        else
+            res("Error. Please try again later. (" + body.message  + ")");
     });
 
-}
-
-function doTest(req, res) {
-    res("Something");
-}
-
-function listofcrap(req, res) {
-    var crap = [
-        {name: "Josh", age: 22},
-        {name: "Bob", age: 40},
-        {name: "whatever", age: 19}
-    ];
-
-    res(crap);
 }
 
 server.start(function() {
