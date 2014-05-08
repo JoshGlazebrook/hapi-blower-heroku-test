4784ad8f6119e7f644b6ca61e7766083428b7711
Josh Glazebrook <josh@joshglazebrook.com>
2014-05-07 22:37:39 -0700

    test

--- app.js
+++ app.js
@@ -12,24 +12,6 @@ server.route([
 
 function sendMessage(req, res) {
 
-
-    /*var message = "finish the fucking project";
-
-    var numbers = [
-        '4257738068',
-        '3605562243',
-        '2066975995',
-        '2064035209'
-    ];*/
-
-    /*numbers.forEach(function(item) {
-        request.post(process.env.BLOWERIO_URL + '/messages', { form: { to: item, message: message }, headers: { 'Accept': 'application/json' } }, function (err, res, body) {
-            console.log(body);
-        });
-
-        res("messages sent");
-    });*/
-
     var data = {
         to: req.payload.number,
         message: req.payload.message
